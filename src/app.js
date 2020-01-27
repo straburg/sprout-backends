841ddb38a603c6f27febe48a69d9fa57a8f7f7b4
coolbop1 <bidoritunmise@gmail.com>
2020-01-27 20:44:14 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ routes(prefix, app);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //App running
 app.listen(port, (req, res) => {
