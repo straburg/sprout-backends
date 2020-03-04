d6d483e3b28116b1a6ddb067ed66b6f12548a9d7
coolbop1 <bidoritunmise@gmail.com>
2020-03-04 14:32:46 +0100

    new develop

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -399,7 +399,7 @@ module.exports = {
                         </style>
                       </head>
                       <body class="">
-                        <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+                        <span class="preheader">Notification from ${bankName} Groups</span>
                         <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
                           <tr>
                             <td>&nbsp;</td>
